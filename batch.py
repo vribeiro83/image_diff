2e5eb321e36cff1c8f7729cb387905c63958a25f
Thuso Simon <thuso@workhorse.uct.ac.za>
2014-06-17 17:24:30 +0200

    make into class

--- batch.py
+++ batch.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#/usr/bin/python2
 
 '''
 time mpiexec -n 8 python batch.py movie_filename
@@ -9,192 +9,153 @@ import scipy as sp
 from scipy import misc
 from scipy import stats
 import mpi4py.MPI as mpi
-import matplotlib.pyplot as plt
+import pylab as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 import datetime as dt
 import os
 import sys
-
-import Image
 import cv2
-
 import glob
 import time
+import ipdb
 
-comm = mpi.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
 
-cap = cv2.VideoCapture(sys.argv[1])
-f_rate = cap.get(cv2.cv.CV_CAP_PROP_FPS)
-frames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-f_sec = 1 / f_rate
-width = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+class Video_class(object):
 
-#split files
-rootfile, ext = sys.argv[1].split(".")
+    def __init__(self, video_path):
+        self.comm = mpi.COMM_WORLD
+        self.size = self.comm.Get_size()
+        self.rank = self.comm.Get_rank()
+        #video info
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
+        self.f_rate = self.cap.get(cv2.cv.CV_CAP_PROP_FPS)
+        self.frames = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        self.f_sec = 1 / self.f_rate
+        self.width = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 
-#CHECK IF FOLDER EXISTS
-temp_folder = "temp_folder2/"
-master_file = temp_folder + "all_averaged.jpg"
+    def video_to_image(self, step_size=1, frame_start=0):
+        '''Takes the videos and outputs frames into images'''
+        #split files
+        rootfile, ext = sys.argv[1].split(".")
 
-if rank == 0:
-    if not os.path.exists(temp_folder):
-        os.makedirs(temp_folder)
+        #CHECK IF FOLDER EXISTS
+        temp_folder = "temp_folder2/"
+        master_file = os.path.join(temp_folder, "all_averaged.jpg")
 
-#h is the step size. Essentially you want one frame at a time!
-h = 1
+        if rank == 0:
+            if not os.path.exists(temp_folder):
+                os.makedirs(temp_folder)
 
-#just a random stating point
-a = 0
+        #h is the step size. Essentially you want one frame at a time!
+        h = 1
+    
+        #just a random stating point
+        a = 0
 
-#local_i is the number of frames each processor will use
-local_i = frames / ( size )
+        #local_i is the number of frames each processor will use
+        local_i = frames / ( size )
 
-#calculate interval that each process handles
-local_startframe = a + ( rank ) * local_i * h
-local_endframe = local_startframe + local_i * h - 1
+        #calculate interval that each process handles
+        local_startframe = a + ( rank ) * local_i * h
+        local_endframe = local_startframe + local_i * h - 1
 
-#print local_startframe , local_endframe , frames
+        #print local_startframe , local_endframe , frames
+    
+        for i in xrange(local_startframe, local_endframe, int(f_rate)):
+            #print i
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i)
+            success,image = cap.read()
+            if i < frames:
+                cv2.imwrite(temp_folder + "frame%.7d.jpg" % i, image)
+        comm.Barrier()
 
-for i in xrange(local_startframe, local_endframe, int(f_rate)):
-    #print i
-    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i)
-    success,image = cap.read()
-    if i < frames:
-      cv2.imwrite(temp_folder + "frame%.7d.jpg" % i, image)
-    comm.Barrier()
-
-'''
-CREATE MASTER IMAGE
-
-THIS PART WAS TAKEN FROM:
-http://stackoverflow.com/questions/9251580/stacking-astronomy-images-with-python
-'''
-
-files = glob.glob(temp_folder + "/frame*.jpg")
-files.sort()
-del files[-1]
-#print len(files)
-
-print "I am about to start creating master image"
-if rank == 0:
-    image=Image.open(files[0])
-    im=np.array(image,dtype=np.float32)
-    for i in range(len(files)):
-        #print i
-        currentimage=Image.open(files[i])
-        im += np.array(currentimage, dtype=np.float32)
-    im /= len(files) # lowered brightness, with magic factor
-    # clip, convert back to uint8:
-    final_image = Image.fromarray(np.uint8(im.clip(0,255)))
-    final_image.save(master_file, 'JPEG')
-else:
-    while not os.path.exists(master_file):
-        print "I, rank %d, am going to sleep..." % rank
-        time.sleep(1)
-
-'''
-Calculate chi squared on the images
-'''
-final_image = misc.imread(master_file)
-frame_no = []
-frame_chi = []
-
-print "About to start doing Chi-squared"
-for i_base in xrange(0, len(files), size):
-#for i_base in range(len(files)):
-    i = i_base + rank
-    if i < len(files):
-        print i
-        images = misc.imread(files[i])
-        temp_frame, extention = files[i].split(".")
-        folder, temp_frame = temp_frame.split("frame")
-        temp_frame = float(temp_frame) * f_sec
-        #print i, temp_frame
-        i_shape = images.shape
-        x, y, z = i_shape
-        ddof = x * y
-        chi, pval = stats.mstats.chisquare(final_image,images)
-        reduced = np.sum(chi) / ( ddof )
+    def make_master_im(self, max_frames=-1):
+        '''makes master in memory'''
+        if max_frames < 1:
+            max_frames = self.frames
+        #initalize master image
+        out = []
+        self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+        _, self.master_image = self.cap.read()
+        sample = np.unravel_index(np.random.randint(0, len(self.master_image.ravel())),self.master_image.shape)
+        out.append(self.master_image[sample])
+        self.master_image = np.asarray(self.master_image, dtype=float)
+        for image_no in xrange(1, max_frames):
+            per_done = self.cap.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)*100.
+            #if per_done % 10 == 0:
+            print '%2.0f Percent Done'%per_done, image_no
+            _, temp_image = self.cap.read()
+            out.append(temp_image[sample])
+            temp_image = np.asarray(temp_image, dtype=float)
+            self.master_image += temp_image
+        self.master_image /= float(max_frames)
+        #self.master_image = self.master_image.clip(0, 255)
+        #self.master_image = np.uint8(self.master_image, dtype=np.uint8)
+        return out
+    
+    def image_chi(self, max_frames=-1):
+        '''
+        Calculate chi squared on the images
+        '''
+        if max_frames < 1:
+            max_frames = self.frames
+        
+        self.frame_no = []
+        self.frame_chi = []
+        self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+        print "About to start doing Chi-squared"
+        x, y, z = self.master_image.shape
+        ddof = x*y
+        for i_base in xrange(0, max_frames):
+            per_done = self.cap.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)*100.
+            #if per_done % 10 == 0:
+            print '%2.0f Percent Done'%per_done, i_base
+            print self.cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+            _, images = self.cap.read()
+            # Scale images so there are non nans or inf from chi squared test
+            chi = chisquare(np.float32(images)+1, self.master_image+1)
+            reduced = np.sum(chi) / ( ddof )
+            self.frame_no.append(float(i_base))
+            self.frame_chi.append(float(reduced))
             
-        frame_no.append(float(temp_frame))
-        frame_chi.append(float(reduced))
-
-#print "About to start zipping..."
-with open(temp_folder + 'results.dat', 'a') as outfile:
-    print "About to start zipping... I am rank ", rank
-    zipped = zip(frame_no, frame_chi)
-    np.savetxt(outfile, zipped)
-comm.Barrier()
+        self.frame_no = np.asarray(self.frame_no)
+        self.frame_chi = np.asarray(self.frame_chi)
+        '''#print "About to start zipping..."
+        with open(temp_folder + 'results.dat', 'a') as outfile:
+        print "About to start zipping... I am rank ", rank
+        zipped = zip(frame_no, frame_chi)
+        np.savetxt(outfile, zipped)
+        comm.Barrier()'''
     #THIS FILE OUTPUT WILL BE USED FOR POSTPROCESSING IF NEEDS BE
 
+    def plot(self):
+        print "I am about to start drawing those amazing figures you really want to see!"
+        majorLocator   = MultipleLocator(5)
+        majorFormatter = FormatStrFormatter('%d')
+        minorLocator   = MultipleLocator(1)
 
-if rank == 0:
-    print "I am about to start drawing those amazing figures you really want to see!"
-    majorLocator   = MultipleLocator(5)
-    majorFormatter = FormatStrFormatter('%d')
-    minorLocator   = MultipleLocator(1)
-
-    outfile = temp_folder + 'results.dat'
-    frame_no, frame_chi = np.loadtxt(outfile, usecols=(0,1), unpack=True)
-    zipped = zip(frame_no, frame_chi)
-    zipped.sort()
-    np.savetxt(outfile, zipped)
-    frame_no, frame_chi = np.loadtxt(outfile, usecols=(0,1), unpack=True)
-
-    #frame_no, frame_chi = zip(zipped)
-    #print frame_chi.min
-
-    frameTime = frame_no / 60
-    redChi = frame_chi
-    
-    minTime, maxTime = min(frameTime), max(frameTime)
-    minChi, maxChi = min(redChi), max(redChi)
-    averageChi = float(sum(redChi) / len(redChi))
-
-    plt.axhline(y=3*minChi)
-
-    ax = plt.subplot(1, 1, 1)
-    #plt.figure()
-    ax.xaxis.set_major_locator(majorLocator)
-    ax.xaxis.set_major_formatter(majorFormatter)
-    ax.xaxis.set_minor_locator(minorLocator)
-
-    plt.tick_params(axis='both', which='major', length=10)
-    plt.tick_params(axis='both', which='minor', length=5)
-    
-    plt.ylim([minChi, maxChi])
-    plt.xlim([minTime, maxTime])
-    plt.xlabel("Time (min)")
-    plt.ylabel("Reduced Chi-squared")
-
-    plt.gca().set_yscale('log')
-
-    xpos, ypos = float(minTime), float(maxChi/1.5)
-    plt.text(xpos, ypos, rootfile)
-
-    plt.plot(frameTime,redChi, 'r-')
-    #plt.savefig(temp_folder + "reduced.eps")
-    #plt.savefig(rootfile + ".eps")
-    plt.savefig(rootfile + ".jpg")
-
-    for i in range(len(frameTime)):
-        output = (temp_folder + "image%.7d.jpg" % i)
-
-        ax = plt.subplot(1, 1, 1)
+        
+        frame_no, frame_chi = self.frame_no, self.frame_chi
+        frameTime = frame_no / 60.
+        redChi = frame_chi
+        
+        minTime, maxTime = min(frameTime), max(frameTime)
+        minChi, maxChi = min(redChi), max(redChi)
+        averageChi = float(sum(redChi) / len(redChi))
 
         plt.axhline(y=3*minChi)
 
+        ax = plt.subplot(1, 1, 1)
         #plt.figure()
         ax.xaxis.set_major_locator(majorLocator)
         ax.xaxis.set_major_formatter(majorFormatter)
         ax.xaxis.set_minor_locator(minorLocator)
-
+    
         plt.tick_params(axis='both', which='major', length=10)
         plt.tick_params(axis='both', which='minor', length=5)
-    
+        
         plt.ylim([minChi, maxChi])
         plt.xlim([minTime, maxTime])
         plt.xlabel("Time (min)")
@@ -203,41 +164,87 @@ if rank == 0:
         plt.gca().set_yscale('log')
 
         xpos, ypos = float(minTime), float(maxChi/1.5)
-        plt.text(xpos, ypos, rootfile)
+        plt.text(xpos, ypos, self.video_path)
 
-        pos = float(i / 60.)
-        #print output, pos
-        plt.axvline(x=pos)
-        plt.plot(frameTime, redChi, 'k-')
-        plt.savefig(output)
-        plt.clf()
+        plt.plot(frameTime,redChi, 'r-')
+        #plt.savefig(temp_folder + "reduced.eps")
+        #plt.savefig(rootfile + ".eps")
+        #ipdb.set_trace()
+        plt.show()
+        plt.savefig(self.video_path + ".jpg")
+    
+        for i in xrange(len(frameTime)):
+            output = (temp_folder + "image%.7d.jpg" % i)
 
-'''
-TO CREATE MOVIE
-http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
-'''
+            ax = plt.subplot(1, 1, 1)
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-out = cv2.VideoWriter(rootfile + '.avi',fourcc, 1, (width,height))
+            plt.axhline(y=3*minChi)
 
-if rank == 0:
-    print "Making a movie"
-    x_offset=y_offset= 0
-    files = glob.glob(temp_folder + "/frame*.jpg")
-    files.sort()
-    del files[-1]
-    output = glob.glob(temp_folder + "image*.jpg")
-    output.sort()
-    for i in xrange(0, len(files), 1):
-        if frame_chi[i] > 3*minChi:
-            images = cv2.imread(files[i])
-            s_img = cv2.imread(output[i])
-            s_img = cv2.resize(s_img, (0,0), fx=0.5, fy=0.5) 
-            images[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
-            out.write(images)
+            #plt.figure()
+            ax.xaxis.set_major_locator(majorLocator)
+            ax.xaxis.set_major_formatter(majorFormatter)
+            ax.xaxis.set_minor_locator(minorLocator)
 
-cap.release()
-cv2.destroyAllWindows()
-out.release()
+            plt.tick_params(axis='both', which='major', length=10)
+            plt.tick_params(axis='both', which='minor', length=5)
+        
+            plt.ylim([minChi, maxChi])
+            plt.xlim([minTime, maxTime])
+            plt.xlabel("Time (min)")
+            plt.ylabel("Reduced Chi-squared")
 
+            plt.gca().set_yscale('log')
+    
+            xpos, ypos = float(minTime), float(maxChi/1.5)
+            plt.text(xpos, ypos, rootfile)
+
+            pos = float(i / 60.)
+            #print output, pos
+            plt.axvline(x=pos)
+            plt.plot(frameTime, redChi, 'k-')
+            plt.savefig(output)
+            plt.clf()
+
+    def make_moive(self):
+        '''
+    TO CREATE MOVIE
+    http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+    '''
+
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.cv.CV_FOURCC(*'XVID')
+        out = cv2.VideoWriter(rootfile + '.avi',fourcc, 1, (width,height))
+
+        print "Making a movie"
+        x_offset=y_offset= 0
+        files = glob.glob(temp_folder + "/frame*.jpg")
+        files.sort()
+        del files[-1]
+        output = glob.glob(temp_folder + "image*.jpg")
+        output.sort()
+        for i in xrange(0, len(files), 1):
+            if frame_chi[i] > 3*minChi:
+                images = cv2.imread(files[i])
+                s_img = cv2.imread(output[i])
+                s_img = cv2.resize(s_img, (0,0), fx=0.5, fy=0.5) 
+                images[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
+                out.write(images)
+
+    def finialize(self):
+        '''Closes everything'''
+        self.cap.release()
+        cv2.destroyAllWindows()
+        out.release()
+
+def chisquare(f_obs, f_exp, axis=0):
+    '''calculates chi squared test correctly for 3D arrays'''
+    return (f_obs - f_exp)**2 / f_exp
+
+if __name__ == '__main__':
+
+    video = Video_class('00016.MTS')
+    freq = video.make_master_im(30)
+    #plt.hist(freq, 5)
+    #plt.show()
+    video.image_chi(30)
+    video.plot()
