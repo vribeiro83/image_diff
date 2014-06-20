95f337115edd65460881e5080713ae6de7d2e389
Thuso Simon <drdangersimon@gmail.com>
2014-06-20 09:33:10 +0200

    added multiprocess support

--- __init__.py
+++ __init__.py
@@ -35,21 +35,95 @@ class Video_class(object):
         self.width = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 
-        #h is the step size. Essentially you want one frame at a time!
-        self.h = 1
+    def _get_inital_master(self, samples):
+        '''samples randomly from the video to get a inital master frame'''
+        # Get empty frame
+        self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+        _, inital_frame = self.cap.read()
+        inital_frame = np.float32(inital_frame *0)
+        # get random samples
+        sam_array = np.sort(np.random.randint(0, self.frames, samples))
+        count = 0.
+        for frame_no in sam_array:
+            self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_no)
+            # get frame
+            sucess, frame = self.cap.read()
+            if not sucess:
+                continue
+            count += 1
+            inital_frame += frame
+        inital_frame /= count
+        return inital_frame
 
-        #just a random stating point
-        self.a = 0
+    def parallel_moving_ave(self, frame_array):
+        '''Does same as moving_ave_chi but in parrallel'''
+        # Check for master image
+        if not 'moving_ave_frame' in vars(self):
+            self.moving_ave_frame = self._get_inital_master(120)
 
-        #local_i is the number of frames each processor will use
-        self.local_i = self.frames / ( self.size )
+        self.frame_no = frame_array
+        self.frame_chi = []
+        self.frame_time = []
+        # start chi square
+        x,y,z = self.moving_ave_frame.shape
+        ddof = float(x * y)
+        for frame_no in frame_array:
+            self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_no)
+            success, frame = self.cap.read()
+            if not success:
+                # remove frame_no
+                self.frame_no.pop(self.frame_no.index(frame_no))
+                continue
+            self.frame_time.append(self.cap.get(cv2.cv.CV_CAP_PROP_POS_MSEC))
+            # Cal chi
+            self.frame_chi.append(chisquare(frame, self.moving_ave_frame).sum()/ddof)
+            # Cal new ave frame
+            cv2.accumulateWeighted(frame, self.moving_ave_frame, 0.020)
 
-        #calculate interval that each process handles
-        self.local_startframe = self.a + ( self.rank ) * self.local_i * self.h
-        self.local_endframe = self.local_startframe + self.local_i * self.h - 1
+        return self.frame_no, self.frame_time, self.frame_chi
+        
+    
+    def moving_ave_chi(self, max_frames=-1, fps=1, plot=False):
+        '''Uses moving average for master frame and calculates fps frames per second'''
+        if max_frames < 1:
+            max_frames = self.frames
+        # set number of steps to skip
+        stride = int(round(self.f_rate / float(fps)))
+        # get inital aveage frame
+        self.moving_ave_frame = self._get_inital_master(120)
+        
+        # out puts
+        self.frame_no = []
+        self.frame_chi = []
+        self.frame_time = []
+        # start chi square
+        x,y,z = self.moving_ave_frame.shape
+        ddof = float(x * y)
+        for frame_no in xrange(0, max_frames, stride):
+            # Get chi square
+            self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_no)
+            success, frame = self.cap.read()
+            if not success:
+                continue
+            self.frame_no.append(frame_no)
+            self.frame_time.append(self.cap.get(cv2.cv.CV_CAP_PROP_POS_MSEC))
+            # Cal chi
+            self.frame_chi.append(chisquare(frame, self.moving_ave_frame).sum()/ddof)
+            # Cal new ave frame
+            cv2.accumulateWeighted(frame, self.moving_ave_frame, 0.020)
+            # Show moving Ave Frame and chi plot
+            if plot:
+                cv2.imshow('Ave Frame',self.moving_ave_frame)
+                #cv2.waitKey(100)
 
-        #print self.local_startframe , self.local_endframe , self.rank
-
+    def _msec2min(self, msec):
+        '''Converts miliseconds to min'''
+        return msec * 1.6666666666666667e-05
+    
+    def _msec2sec(self, msec):
+        '''Converts mili-sec to sec'''
+        return msec * 10**-3
+        
     def make_master_im(self, max_frames=-1, step=-1):
         '''makes master in memory'''
         if max_frames < 1 and step < 1:
@@ -64,27 +138,20 @@ class Video_class(object):
 
         #print self.local_i, step, max_frames / self.size
         #initalize master image
-        out = []
+        
         _, self.master_image = self.cap.read()
         sample = np.unravel_index(np.random.randint(0, len(self.master_image.ravel())),self.master_image.shape)
-        out.append(self.master_image[sample])
         self.master_image = np.asarray(self.master_image, dtype=float)
         #for image_no in xrange(1, max_frames):
         for image_no in xrange(self.local_startframe, self.local_endframe, step):
             self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, image_no)
             print image_no, step, self.local_startframe, self.local_endframe, self.frames
-            #per_done = self.cap.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)*100.
-            #per_done = self.cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)100.
-            #if per_done % 10 == 0:
-            #print '%2.0f Percent Done'%per_done, image_no
             _, temp_image = self.cap.read()
             out.append(temp_image)
             temp_image = np.asarray(temp_image, dtype=float)
             self.master_image += temp_image
         self.master_image /= float(max_frames)
-        #self.master_image = self.master_image.clip(0, 255)
-        #self.master_image = np.uint8(self.master_image, dtype=np.uint8)
-        return out
+        
     
     def image_chi(self, max_frames=-1, step=-1):
         '''
@@ -107,10 +174,6 @@ class Video_class(object):
         for i_base in xrange(self.local_startframe, self.local_endframe, step):
             self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i_base)
             print i_base, step, self.local_startframe, self.local_endframe, self.frames 
-            #per_done = self.cap.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)*100.
-            #if per_done % 10 == 0:
-            #print '%2.0f Percent Done'%per_done, i_base
-            #print self.cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
             _, images = self.cap.read()
             # Scale images so there are non nans or inf from chi squared test
             chi = chisquare(np.float32(images)+1, self.master_image+1)
@@ -137,12 +200,14 @@ class Video_class(object):
         minorLocator   = MultipleLocator(1)
 
         frame_no, frame_chi = self.frame_no, self.frame_chi
-        frameTime = frame_no / 60.
+        # in mili seconds
+        frameTime = np.asarray(self.frame_time)
         redChi = frame_chi
 
         self.dpi = 600
-
-        minTime, maxTime = min(frameTime), max(frameTime)
+        # time in min
+        #ipdb.set_trace()
+        minTime, maxTime = self._msec2min(min(frameTime)), self._msec2min(max(frameTime))
         minChi, maxChi = min(redChi), max(redChi)
         averageChi = float(sum(redChi) / len(redChi))
 
@@ -157,28 +222,29 @@ class Video_class(object):
         plt.tick_params(axis='both', which='major', length=10)
         plt.tick_params(axis='both', which='minor', length=5)
         
-        #plt.ylim([minChi, maxChi])
-        #plt.xlim([minTime, maxTime])
-        #plt.xlabel("Time (min)")
-        #plt.ylabel("Reduced Chi-squared")
+        plt.ylim([minChi, maxChi])
+        plt.xlim([minTime, maxTime])
+        plt.xlabel("Time (min)")
+        plt.ylabel("Reduced Chi-squared")
 
         plt.gca().set_yscale('log')
 
         xpos, ypos = float(minTime), float(maxChi/1.5)
         plt.text(xpos, ypos, self.video_path)
 
-        if individual == False:
+        if not individual:
             if self.rank == 1:
-                plt.plot(frameTime,redChi, 'r-')
+                plt.plot(self._msec2min(frameTime),redChi, 'r-')
                 plt.savefig(self.video_path + ".jpg", dpi=self.dpi)
                 plt.clf()
-        elif individual == True:
+        else:
             for i in xrange(len(frameTime)):
-                pos = float(i / 60.)
+                pos = float(i)
                 plt.axvline(x=pos)      
-                plt.plot(frameTime, redChi, 'k-')
-                plt.savefig(output, dpi=self.dpi)
-                plt.clf()
+            plt.plot(self._msec2min(frameTime), redChi, 'k-')
+            plt.savefig(self.video_path + ".jpg", dpi=self.dpi)
+            plt.show()
+            plt.clf()
         #plt.savefig(temp_folder + "reduced.eps")
         #plt.savefig(rootfile + ".eps")
         #ipdb.set_trace()
@@ -243,18 +309,63 @@ class Video_class(object):
         cv2.destroyAllWindows()
         out.release()
 
+    def mpi_work_split(self, max_frames=-1, size=None, fps=1, start=0):
+        '''Splits work up from worker size'''
+        if not size is None and size > 1:
+            self.size = size
+        if max_frames < 1:
+            max_frames = self.frames
+        
+        stride = int(round(self.f_rate / float(fps)))
+        # Make split array
+        split = np.linspace(start, max_frames, self.size+1).round()
+        frame_min, frame_max = int(split[self.rank]),int(split[self.rank + 1])
+        return range(frame_min, frame_max, stride)
+        
+
+    def aggrigate_to_root(self, frame_no, frame_time, frame_chi):
+        '''Send and organizes data at root'''
+        if self.rank == 0:
+            reciv =[(frame_no, frame_time, frame_chi)]
+            for source in xrange(1,self.size):
+                reciv.append(self.comm.recv(source=source))
+                print 'Recived from process %i'%source
+            self.frame_no, self.frame_time, self.frame_chi = np.hstack(reciv)
+
+        else:
+            self.comm.send((frame_no, frame_time, frame_chi), dest=0)
+            
+        
+    
 def chisquare(f_obs, f_exp, axis=0):
     '''calculates chi squared test correctly for 3D arrays'''
     return (f_obs - f_exp)**2 / f_exp
 
+
+
 if __name__ == '__main__':
 
-    # Set the Number of frames wished
-    frames = 30
+    # Test multiprocessing
+    #frames = 10000
     video = Video_class('00016.MTS')
-    freq = video.make_master_im(frames)
+    # Single process
+    if video.rank == 0:
+        t_single = mpi.Wtime()
+        video.moving_ave_chi()
+        t_single -= mpi.Wtime()
+        del video.moving_ave_frame
+    video.comm.barrier()
+    # Multiprocess
+    t_multi = mpi.Wtime()
+    work_array = video.mpi_work_split()
+    frame_no, frame_time, frame_chi = video.parallel_moving_ave(work_array)
+    video.aggrigate_to_root(frame_no, frame_time, frame_chi)
+    video.comm.barrier()
+    t_multi -= mpi.Wtime()
+    if video.rank == 0:
+        print 'Single time is %2.0f and Multi time is %2.0f for speed up of %2.1f times'%(abs(t_single),abs(t_multi), t_multi/t_single)
     #plt.hist(freq, 5)
     #plt.show()
-    video.image_chi()
-    #video.plot()
+    #video.image_chi()
+    video.plot()
     #video.make_movie()
